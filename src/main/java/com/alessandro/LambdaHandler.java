6a33c0e6c0d52e43b6dc2397faa9bbd2b32b92af
Okairis <alessandroretuerto@hotmail.com>
2025-05-29 19:02:03 -0500

    remove

--- src/main/java/com/alessandro/LambdaHandler.java
+++
@@ -1,7 +0,0 @@
-package com.alessandro;
-
-import com.alessandro.dto.FileUploadRequest;
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class LambdaHandler extends SpringBootRequestHandler<FileUploadRequest, String> {
-}
